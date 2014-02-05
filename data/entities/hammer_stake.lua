51b34c8f80fd10b9214ce3c03b80c60adac605fc
Christopho <christopho@solarus-games.org>
2014-02-05 16:44:39 +0100

    Working on the hammer.

--- data/entities/hammer_stake.lua
+++ data/entities/hammer_stake.lua
@@ -1,4 +1,54 @@
 local stake = ...
 
--- TODO
+local function test_collision_with_hero_hammer(entity)
+
+  if entity:get_type() ~= "hero" then
+    -- Ignore collisions with entities other than the hero.
+    return false
+  end
+
+  if hero:get_animation() != "hammer" then
+    -- Don't bother testing collisions if the hero is not currently
+    -- using the hammer.
+    return false
+  end
+
+  -- The hero is using the hammer. Determine the exact point to test.
+  local hero_direction = entity:get_direction()
+  local x, y = entity:get_center_position()
+  if hero_direction == 0 then
+    -- Right.
+    x = x + 16
+  elseif hero_direction == 1 then
+    -- Up.
+    y = y - 16
+  elseif hero_direction == 2 then
+    -- Left.
+    x = x - 16
+  else
+    -- Down.
+    y = y + 16
+  end
+
+  -- Test if this point overlaps the stake.
+  -- TODO add a function entity:get_bounding_box()
+  -- TODO add a function entity:overlaps(x, y, [width, height])
+  -- and then just do: return stake:overlaps(x, y)
+  local stake_x, stake_y = stake:get_position()
+  local stake_origin_x, stake_origin_y = stake:get_origin()
+  local stake_top_left_x, stake_top_left_y = stake_x - stake_origin_x, stake_y - stake_origin_y
+  local stake_width, stake_height = stake:get_size()
+
+  return x >= stake_top_left_x and x < stake_top_left_x + stake_width
+      and y >= stake_top_left_y and y < stake_top_left_y + stake_height
+end
+
+stake:add_collision_detector(test_collision_with_hero_hammer, function(entity)
+
+  -- Change the animation to down.
+  stake:get_sprite():set_animation("down")
+
+  -- Tell the hammer it has just successfully pushed something.
+  game:get_item("hammer"):set_pushed_stake(true)
+end
 
--- data/items/hammer.lua
+++ data/items/hammer.lua
@@ -5,6 +5,7 @@ function item:on_created()
 
   item:set_savegame_variable("possession_hammer")
   item:set_assignable(true)
+  item:set_pushed_stake(false)
 end
 
 function item:on_using()
@@ -14,9 +15,23 @@ function item:on_using()
     hero:unfreeze()
   end)
 
-  -- TODO detect collision with hammer stakes
-
-  sol.audio.play_sound("hammer")  -- TODO play "hammer_stake" instead if success
+  item:set_pushed_stake(false)
+  sol.timer.start(item, 50, function()
+    if item:has_pushed_stake() then
+      sol.audio.play_sound("hammer_stake")  -- Successfully pushed a stake.
+    else
+      sol.audio.play_sound("hammer")  -- Nothing was pushed.
+    end
+    item:set_pushed_stake(false)
+  end)
  
+end
+
+function item:has_pushed_stake()
+  return item.pushed_stake
+end
+
+function item:set_pushed_stake(pushed_stake)
+  item.pushed_stake = pushed_stake
 end
 
