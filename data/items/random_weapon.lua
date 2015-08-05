a2290d3675a3ca3109af5f2b9e646f0c08bab8a8
Christopho <christopho@solarus-games.org>
2015-08-05 21:24:04 +0200

    Merge Vincent's changes

--- data/items/random_weapon.lua
+++ data/items/random_weapon.lua
@@ -2,16 +2,22 @@ local item = ...
 
 -- When it is created, this item creates another item randomly chosen
 -- and then destroys itself.
--- The random item is often a heart.
+-- The random item is often some rupees.
 
 -- Probability of each item between 0 and 1000.
 local probabilities = {
-  [{ "bomb", 1 }]       = 5,    -- 1 bomb.
-  [{ "arrow", 1 }]      = 5,    -- 1 arrow.
+  [{ "bomb", 1 }]       = 150,    -- 1 bomb.
+  [{ "bomb", 2 }]       = 75,    -- 3 bombs.
+  [{ "bomb", 3 }]       = 25,    -- 8 bombs.
+  [{ "arrow", 1 }]      = 150,    -- 1 arrow.
+  [{ "arrow", 2 }]      = 75,    -- 5 arrows.
+  [{ "arrow", 3 }]      = 25,    -- 10 arrows.
   [{ "rupee", 1 }]      = 50,   -- 1 rupee.
   [{ "rupee", 2 }]      = 15,   -- 5 rupees.
-  [{ "magic_flask", 1}] = 25,    -- Small magic jar.
-  [{ "heart", 1}]       = 500,  -- Heart.
+  [{ "rupee", 3 }]      = 5,    -- 20 rupees.
+  [{ "magic_flask", 1}] = 25,   -- Small magic jar.
+  [{ "magic_flask", 2}] = 8,    -- Big magic jar.
+  [{ "heart", 1}]       = 5,  -- Heart.
 }
 
 function item:on_pickable_created(pickable)
