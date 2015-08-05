725798d8069ec4a7e84baab9bc583a802d789a33
Christopho <christopho@solarus-games.org>
2015-08-05 02:02:12 +0200

    Two more houses in Kakariko village

--- data/items/bottle.lua
+++ data/items/bottle.lua
@@ -1,100 +1,84 @@
 -- This script handles all bottles (each bottle script runs it).
+-- Variants:
+-- 1: empty
+-- 2: red potion
+-- 3: green potion
+-- 4: blue potion
 
 local item = ...
+
+local function drink_potion(variant)
+
+  local game = item:get_game()
+  local map = item:get_map()
+  local hero = map:get_hero()
+
+  hero:freeze()
+  hero:set_animation("drinking")
+  local hero_x, hero_y, hero_layer = hero:get_position()
+  local potion = map:create_custom_entity({
+    direction = 0,
+    x = hero_x,
+    y = hero_y,
+    layer = hero_layer,
+    width = 16,
+    height = 16,
+  })
+  potion:set_drawn_in_y_order(true)
+  local potion_sprite = potion:create_sprite("entities/drinking_potion")
+  potion_sprite:set_animation(variant)
+
+  function potion_sprite:on_animation_finished()
+
+    if variant == 2 or variant == 4 then
+      game:add_life(game:get_max_life())
+    end
+    if variant == 3 or variant == 4 then
+      game:add_magic(game:get_max_magic())
+    end
+    item:set_variant(1)
+    potion:remove()
+    hero:unfreeze()
+  end
+end
 
 function item:on_using()
 
   local variant = self:get_variant()
   local game = self:get_game()
-  local map = self:get_map()
 
   -- empty bottle
   if variant == 1 then
     sol.audio.play_sound("wrong")
-    self:set_finished()
-
-    -- water
-  elseif variant == 2 then
-    -- ask the hero to pour away the water
-    game:start_dialog("use_bottle_with_water", function(answer)
-      if answer == 1 then
-	-- empty the water
-	self:set_variant(1) -- make the bottle empty
-	sol.audio.play_sound("item_in_water")
-      end
-      self:set_finished()
-    end)
+    item:set_finished()
 
     -- red potion
-  elseif variant == 3 then
-    game:add_life(game:get_max_life())
-    self:set_variant(1)
-    self:set_finished()
+  elseif variant == 2 then
+    if game:get_life() >= game:get_max_life() then
+      game:start_dialog("potion_not_needed")
+    else
+      drink_potion(variant)
+    end
+    item:set_finished()
 
     -- green potion
-  elseif variant == 4 then
-    game:add_magic(game:get_max_magic())
-    self:set_variant(1)
-    self:set_finished()
+  elseif variant == 3 then
+    if game:get_magic() >= game:get_max_magic() then
+      game:start_dialog("potion_not_needed")
+    else
+      drink_potion(variant)
+    end
+    item:set_finished()
 
     -- blue potion
-  elseif variant == 5 then
-    game:add_life(game:get_max_life())
-    self:set_variant(1)
-    game:add_magic(game:get_max_magic())
-    self:set_finished()
-
-    -- fairy
-  elseif variant == 6 then
-
-    -- release the fairy
-    local x, y, layer = map:get_entity("hero"):get_position()
-    map:create_pickable{
-      treasure_name = "fairy",
-      treasure_variant = 1,
-      x = x,
-      y = y,
-      layer = layer
-    }
-    self:set_variant(1) -- make the bottle empty
-    self:set_finished()
-  end
-end
-
-function item:on_npc_interaction(npc)
-
-  if npc:get_name():find("^water_for_bottle") then
-    local game = self:get_game()
-    local map = self:get_map()
-    -- The hero interacts with a place where he can get some water.
-    if game:has_bottle() then
-      local first_empty_bottle = game:get_first_empty_bottle()
-      if first_empty_bottle ~= nil then
-        game:start_dialog("found_water", function(answer)
-	  if answer == 1 then
-            local hero = map:get_entity("hero")
-            hero:start_treasure(first_empty_bottle:get_name(), 2, nil)
-	  end
-	end)
-      else
-        game:start_dialog("found_water.no_empty_bottle")
-      end
+  elseif variant == 4 then
+    if game:get_life() >= game:get_max_life() and
+        game:get_magic() >= game:get_max_magic() then
+      game:start_dialog("potion_not_needed")
     else
-      game:start_dialog("found_water.no_bottle")
+      drink_potion(variant)
     end
+    item:set_finished()
+
   end
 end
-
-function item:on_npc_interaction_item(npc, item_used)
-
-  if item_used:get_name():find("^bottle")
-      and npc:get_name():find("^water_for_bottle") then
-    -- the hero interacts with a place where he can get some water:
-    -- no matter whether he pressed the action key or the item key of a bottle, we do the same thing
-    self:on_npc_interaction(npc)
-    return true
-  end
-
-  return false
-end
-
---
+++ data/maps/kakariko/red_potion_house.lua
@@ -0,0 +1,25 @@
+local map = ...
+local game = map:get_game()
+
+local price = 60
+
+function potion_npc:on_interaction()
+
+  local first_empty_bottle = game:get_first_empty_bottle()
+  if first_empty_bottle == nil then
+    game:start_dialog("kakariko.red_potion_house.no_bottle")
+  else
+    game:start_dialog("kakariko.red_potion_house.offer", function(answer)
+      if answer == 3 then
+        if game:get_money() < price then
+          game:start_dialog("not_enough_money")
+        else
+          game:start_dialog("kakariko.red_potion_house.yes", function()
+            game:remove_money(price)
+            hero:start_treasure(first_empty_bottle:get_name(), 2)
+          end)
+        end
+      end
+    end)
+  end
+end
