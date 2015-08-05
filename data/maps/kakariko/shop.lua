513c1046cf336243d3ebc5c5aa7b2689df942a4c
Christopho <christopho@solarus-games.org>
2015-08-05 12:52:53 +0200

    Fix shop

--- data/maps/kakariko/shop.lua
+++ data/maps/kakariko/shop.lua
@@ -9,20 +9,17 @@ function map:on_started()
     if shop_heart ~= nil then
       shop_heart:set_position(48, 64)
     end
-    if shop_heart ~= nil then
-      shop_heart:set_position(96, 64)
-    end
     if shop_small_magic_flask ~= nil then
-      shop_small_magic_flask:set_position(144, 64)
+      shop_small_magic_flask:set_position(96, 64)
     end
     if shop_big_magic_flask ~= nil then
-      shop_big_magic_flask:set_position(192, 64)
+      shop_big_magic_flask:set_position(144, 64)
     end
     if shop_arrow ~= nil then
-      shop_arrow:set_position(240, 64)
+      shop_arrow:set_position(192, 64)
     end
     if shop_bomb ~= nil then
-      shop_bomb:set_position(288, 64)
+      shop_bomb:set_position(240, 64)
     end
   else
     if game:get_value("kakariko_shop_bow_reduced_price") then
@@ -41,6 +38,7 @@ function merchant:on_interaction()
     game:start_dialog("kakariko.shop.reduced_bow", function()
       shop_bow:set_enabled(true)
       shop_bow_1000:set_enabled(false)
+      game:set_value("kakariko_shop_bow_reduced_price", true)
     end)
   else
     game:start_dialog("shop.choose")
