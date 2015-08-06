6f92b0afc267cd06ba81fc489e9e57b088a081c8
Christopho <christopho@solarus-games.org>
2015-08-06 14:44:11 +0200

    Put flying setting again for pikes, we want them to traverse holes

--- data/enemies/pike_auto.lua
+++ data/enemies/pike_auto.lua
@@ -12,7 +12,10 @@ function enemy:on_created()
   self:create_sprite("enemies/pike_auto")
   self:set_size(16, 16)
   self:set_origin(8, 13)
-  self:set_obstacle_behavior("swimming")  -- Traverse water but don't traverse low walls.
+
+  -- Traverse holes and water. But then invisible walls are needed if you want to stop on low walls.
+  self:set_obstacle_behavior("flying")
+
   self:set_can_hurt_hero_running(true)
   self:set_optimization_distance(0)  -- Keep them aligned when there are several ones.
   self:set_invincible()
