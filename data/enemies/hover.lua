31f0238f41963a7ef2a4b40a54ba43c507b18d31
Christopho <christopho@solarus-games.org>
2015-08-06 22:34:35 +0200

    Implement Hover

--- data/enemies/hover.lua
+++ data/enemies/hover.lua
@@ -6,8 +6,8 @@ local properties = {
   sprite = "enemies/" .. enemy:get_breed(),
   life = 2,
   damage = 2,
-  normal_speed = 64,
-  faster_speed = 64,
+  normal_speed = 48,
+  faster_speed = 48,
   obstacle_behavior = "swimming",  -- Allow to traverse water.
 }
 
