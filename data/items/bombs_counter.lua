aecd432861c2b0b9737aa8f6ca085d51d59ceef8
Christopho <christopho@solarus-games.org>
2015-08-06 09:02:22 +0200

    Don't allow to carry bombs

--- data/items/bombs_counter.lua
+++ data/items/bombs_counter.lua
@@ -1,36 +1,38 @@
 local item = ...
+local game = item:get_game()
 
 local sound_timer
 
 function item:on_created()
 
-  self:set_savegame_variable("possession_bomb_counter")
-  self:set_amount_savegame_variable("amount_bomb_counter")
-  self:set_assignable(true)
-  self:set_max_amount(10)
+  item:set_savegame_variable("possession_bomb_counter")
+  item:set_amount_savegame_variable("amount_bomb_counter")
+  item:set_assignable(true)
+  item:set_max_amount(10)
 end
 
 -- Called when the player uses the bombs of his inventory by pressing the corresponding item key.
 function item:on_using()
 
-  if self:get_amount() == 0 then
+  if item:get_amount() == 0 then
     if sound_timer == nil then
       sol.audio.play_sound("wrong")
-      sound_timer = sol.timer.start(item:get_game(), 500, function()
+      sound_timer = sol.timer.start(game, 500, function()
         sound_timer = nil
       end)
     end
   else
-    self:remove_amount(1)
-    local x, y, layer = self:create_bomb()
+    item:remove_amount(1)
+    local x, y, layer = item:create_bomb()
     sol.audio.play_sound("bomb")
   end
-  self:set_finished()
+  item:set_finished()
 end
 
 function item:create_bomb()
 
-  local hero = self:get_map():get_entity("hero")
+  local map = item:get_map()
+  local hero = map:get_entity("hero")
   local x, y, layer = hero:get_position()
   local direction = hero:get_direction()
   if direction == 0 then
@@ -43,10 +45,34 @@ function item:create_bomb()
     y = y + 16
   end
 
-  self:get_map():create_bomb{
+  -- We create the bomb as a custom entity
+  -- rather than a built-in bomb, because we don't
+  -- want it to be liftable.
+  local bomb = map:create_custom_entity({
     x = x,
     y = y,
-    layer = layer
-  }
+    layer = layer,
+    width = 16,
+    height = 16,
+    direction = 0,
+  })
+  local bomb_sprite = bomb:create_sprite("entities/bomb")
+  bomb_sprite:set_animation("stopped")
+
+  local function explode()
+    map:create_explosion({
+      x = x,
+      y = y,
+      layer = layer,
+    })
+    sol.audio.play_sound("explosion")
+    bomb:remove()
+  end
+
+  -- Schedule the explosion
+  sol.timer.start(bomb, 3000, function()
+    bomb_sprite:set_animation("stopped_explosion_soon")
+    sol.timer.start(bomb, 1500, explode)
+  end)
 end
 
