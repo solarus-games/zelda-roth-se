327a6933418bfcf2bd0778d8b87052a7d5f8bff8
Christopho <christopho@solarus-games.org>
2015-08-06 12:57:13 +0200

    Implement Armos

--- data/enemies/armos.lua
+++ data/enemies/armos.lua
@@ -1,18 +1,16 @@
 local enemy = ...
 
--- Keese: a bat that sleeps until the hero gets close.
+-- Armos: a statue that sleeps until the hero gets close.
 
 local behavior = require("enemies/lib/waiting_for_hero")
 
 local properties = {
   sprite = "enemies/" .. enemy:get_breed(),
-  life = 1,
-  damage = 1,
+  life = 5,
+  damage = 2,
   normal_speed = 64,
   faster_speed = 64,
-  ignore_obstacles = true,
-  obstacle_behavior = "flying",
-  movement_create = function() end,  -- No movement.
+  waking_distance = 72,
 }
 
 behavior:create(enemy, properties)
