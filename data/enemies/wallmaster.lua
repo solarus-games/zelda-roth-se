6902df5a611094aa282fc0ee65489c4ed83d194b
Christopho <christopho@solarus-games.org>
2015-08-07 15:26:41 +0200

    Wallmaster is now working

--- data/enemies/wallmaster.lua
+++ data/enemies/wallmaster.lua
@@ -3,8 +3,8 @@ local enemy = ...
 local map = enemy:get_map()
 
 local sprite
-local shadow_sprite
-local shadow_xy
+local shadow
+local grabbed_hero
 
 function enemy:on_created()
 
@@ -15,33 +15,35 @@ function enemy:on_created()
   enemy:set_obstacle_behavior("flying")
   enemy:set_can_hurt_hero_running(true)
   enemy:set_layer_independent_collisions(true)
+  enemy:set_optimization_distance(0)
   enemy:set_invincible()
   sprite = enemy:create_sprite("enemies/" .. enemy:get_breed())
   sprite:set_animation("sleeping")
-  shadow_sprite = sol.sprite.create("enemies/" .. enemy:get_breed())
-  shadow_sprite:set_animation("shadow")
 end
 
 function enemy:on_restarted()
 
+  if shadow ~= nil then
+    shadow:remove()
+  end
+  grabbed_hero = false
   sprite:set_animation("sleeping")
-  shadow_xy = nil
 
   sol.timer.start(enemy, 5000, function()
 
     local distance = 240  -- Make sure that we start outside the visible screen.
     local hero = map:get_hero()
-    local hero_x, hero_y = hero:get_position()
-    enemy:set_position(hero_x, hero_y - distance, 2)
-    shadow_xy = { hero_x, hero_y }
+    local hero_x, hero_y, hero_layer = hero:get_position()
+    enemy:set_position(hero_x, hero_y, 2)
+    sprite:set_xy(0, -distance)  -- Display the sprite with an offset.
 
-    -- Get down towards the hero.
+    -- Move the sprite towards the hero.
     local movement = sol.movement.create("straight")
     movement:set_speed(192)
     movement:set_angle(3 * math.pi / 2)
     movement:set_ignore_obstacles(true)
     movement:set_max_distance(distance)
-    movement:start(enemy)
+    movement:start(sprite)
 
     function movement:on_finished()
       sol.timer.start(enemy, 500, function()
@@ -50,34 +52,52 @@ function enemy:on_restarted()
     end
 
     sprite:set_animation("walking")
-    
+
+    -- Create a shadow below the hero.
+    -- The hand is above, this is why we need two entities.
+    shadow = map:create_custom_entity({
+      x = hero_x,
+      y = hero_y,
+      layer = hero_layer,
+      direction = 0,
+    })
+    local shadow_sprite = shadow:create_sprite("enemies/" .. enemy:get_breed())
+    shadow_sprite:set_animation("shadow")
+
     sol.timer.start(enemy, 500, function()
       sol.audio.play_sound("jump")
     end)
 
     -- Go back.
     sol.timer.start(enemy, 3000, function()
-      movement:set_angle(math.pi / 2)
-      movement:set_speed(192)
-      movement:start(enemy)
-      function movement:on_finished()
-        enemy:restart()
+      if not grabbed_hero then
+        movement:set_angle(math.pi / 2)
+        movement:set_speed(192)
+        movement:start(sprite)
+        function movement:on_finished()
+          enemy:restart()
+        end
       end
     end)
   end)
 end
 
 -- Function called when overlapping the hero.
-function enemy:on_attacking_hero(hero)
+function enemy:on_attacking_hero(hero, enemy_sprite)
 
-  local movement = enemy:get_movement()
+  local movement = sprite:get_movement()
   if movement ~= nil and movement:get_speed() ~= 0 then
     -- Currently moving: don't grab the hero now.
     return
   end
 
+  if enemy_sprite ~= sprite then
+    -- Not the hand sprite but the shadow.
+    return
+  end
+
   if sprite:get_animation() ~= "closed" then
-    -- Not the hand grabbing animation.
+    -- Not the hand grabbing.
     return
   end
 
@@ -88,6 +108,7 @@ function enemy:on_attacking_hero(hero)
 
   -- Teleport the hero.
   -- TODO if teleporting to the same map, the map is not reset, take care of separator regions
+  grabbed_hero = true
   hero:freeze()
   hero:set_invincible(true, 500)
   sol.timer.start(hero, 500, function()
@@ -103,11 +124,4 @@ function enemy:on_attacking_hero(hero)
       end
     end)
   end)
-end
-
-function enemy:on_pre_draw(dst_surface)
-
-  if shadow_xy ~= nil then
-    map:draw_sprite(shadow_sprite, shadow_xy[1], shadow_xy[2])
-  end
 end
