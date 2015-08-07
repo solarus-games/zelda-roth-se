e3090fb8147abcc0100d7e5bd36a63dbf1498647
Christopho <christopho@solarus-games.org>
2015-08-07 20:46:14 +0200

    Implement Chasupa

---
+++ data/enemies/chasupa.lua
@@ -0,0 +1,18 @@
+local enemy = ...
+
+-- Chasupa: a flying eye that sleeps until the hero gets close.
+
+local behavior = require("enemies/lib/waiting_for_hero")
+
+local properties = {
+  sprite = "enemies/" .. enemy:get_breed(),
+  life = 2,
+  damage = 2,
+  normal_speed = 64,
+  faster_speed = 64,
+  ignore_obstacles = true,
+  obstacle_behavior = "flying",
+  waking_distance = 200,
+}
+
+behavior:create(enemy, properties)
