63b11f3dd9670417ea9cf76933077a201e36234b
Christopho <christopho@solarus-games.org>
2015-08-10 21:16:50 +0200

    Dungeon 9

---
+++ data/maps/dungeon_9/1f.lua
@@ -0,0 +1,14 @@
+local map = ...
+local game = map:get_game()
+
+local door_manager = require("maps/lib/door_manager")
+door_manager:manage_map(map)
+local separator_manager = require("maps/lib/separator_manager")
+separator_manager:manage_map(map)
+
+function map:on_opening_transition_finished(destination)
+
+  if destination == from_outside then
+    game:start_dialog("dungeon_9.welcome")
+  end
+end
