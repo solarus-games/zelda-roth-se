4c49e58456787b5149269d194ae40bff17826e5d
Christopho <christopho@solarus-games.org>
2015-08-11 02:44:51 +0200

    Dungeon 9

--- data/entities/zelda.lua
+++ data/entities/zelda.lua
@@ -1,15 +1,20 @@
 local zelda = ...
 
 local game = zelda:get_game()
+local map = game:get_map()
 local sprite = zelda:get_sprite()
 local hero = game:get_hero()
-local following = false
 local movement
 
 zelda:set_optimization_distance(0)
 zelda:set_traversable_by(true)
+zelda:set_can_traverse("enemy", true)
 zelda:set_can_traverse("hero", true)
+zelda:set_can_traverse("npc", true)
+zelda:set_can_traverse("sensor", true)
 zelda:set_can_traverse("separator", true)
+zelda:set_can_traverse("stairs", true)
+zelda:set_can_traverse("teletransporter", true)
 
 -- TODO make this available to other scripts.
 local function follow_hero()
@@ -17,9 +22,8 @@ local function follow_hero()
   movement = sol.movement.create("target")
   movement:set_speed(64)
   movement:start(zelda)
-  following = true
+  game.zelda_following = true
   sprite:set_animation("walking")
-
 end
 
 -- Stops for now because too close or too far.
@@ -30,13 +34,25 @@ local function stop_following_hero()
   sprite:set_animation("stopped")
 end
 
+function zelda:on_created()
+
+  if game.zelda_following then
+
+    zelda:set_position(hero:get_position())
+    follow_hero()
+    movement:set_ignore_obstacles(true)
+  elseif map:get_id() ~= "dungeon_9/b2" then
+    zelda:remove()
+  end
+end
+
 function zelda:on_interaction()
 
   if not game:get_value("dungeon_9_found_zelda") then
     game:start_dialog("dungeon_9.zelda.hello")
     game:set_value("dungeon_9_found_zelda", true)
     follow_hero()
-  elseif not following then
+  elseif not game.zelda_following then
     game:start_dialog("dungeon_9.zelda.angry")
     follow_hero()
   end
@@ -55,7 +71,7 @@ end
 function zelda:on_position_changed()
 
   local distance = zelda:get_distance(hero)
-  if following and distance < 32 then
+  if game.zelda_following and zelda:is_very_close_to_hero() then
     -- Close enough to the hero: stop.
     stop_following_hero()
   end
@@ -72,17 +88,32 @@ function zelda:on_movement_finished()
   sprite:set_animation("stopped")
 end
 
-sol.timer.start(zelda, 200, function()
+function zelda:is_very_close_to_hero()
 
   local distance = zelda:get_distance(hero)
-  if following then
-    if movement == nil and distance >= 32 and distance < 100 then
+  return distance < 32
+end
+
+function zelda:is_far_from_hero()
+
+  local distance = zelda:get_distance(hero)
+  return distance >= 100
+end
+
+sol.timer.start(zelda, 50, function()
+
+  if game.zelda_following then
+    if movement == nil and not zelda:is_very_close_to_hero() and not zelda:is_far_from_hero() then
       -- Restart.
       follow_hero()
-    elseif movement ~= nil and distance >= 100 then
+    elseif movement ~= nil and zelda:is_far_from_hero() then
       -- Too far: stop.
       stop_following_hero()
     end
+  end
+
+  if hero:get_state() == "stairs" then
+    zelda:set_position(hero:get_position())
   end
 
   return true
--- data/maps/lib/separator_manager.lua
+++ data/maps/lib/separator_manager.lua
@@ -10,6 +10,8 @@ function separator_manager:manage_map(map)
 
   local enemy_places = {}
   local destructible_places = {}
+  local game = map:get_game()
+  local zelda = map:get_entity("zelda")
 
   -- Function called when a separator was just taken.
   local function separator_on_activated(separator)
@@ -48,6 +50,11 @@ function separator_manager:manage_map(map)
       if not block:is_in_same_region(hero) then
         block:reset()
       end
+    end
+
+    -- Make Zelda follow directly the hero when taking a separator.
+    if zelda ~= nil and game.zelda_following and not zelda:is_far_from_hero() then
+      zelda:set_position(hero:get_position())
     end
   end
 
@@ -144,6 +151,7 @@ function separator_manager:manage_map(map)
       destructible = destructible,
     }
   end
+
 end
 
 return separator_manager
