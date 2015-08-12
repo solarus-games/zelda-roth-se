4fad05ceb7dd5c43f34a7bd88689177d8189466a
Christopho <christopho@solarus-games.org>
2015-08-12 23:10:21 +0200

    Fix closed door in dungeon 6
    Closes #22

--- data/maps/dungeon_6/1f.lua
+++ data/maps/dungeon_6/1f.lua
@@ -6,6 +6,13 @@ door_manager:manage_map(map)
 local separator_manager = require("maps/lib/separator_manager")
 separator_manager:manage_map(map)
 
+function map:on_started(destination)
+
+  if destination ~= from_outside_s then
+    map:set_doors_open("auto_door_b", true)
+  end
+end
+
 function map:on_opening_transition_finished(destination)
 
   if destination == from_outside_s then
