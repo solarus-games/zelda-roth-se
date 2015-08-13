591f467527bb6093230805ed9e9eaebb1311ac6d
Christopho <christopho@solarus-games.org>
2015-08-13 21:54:05 +0200

    Dungeon 7: fix hero stuck because of closed door

--- data/maps/dungeon_7/1f.lua
+++ data/maps/dungeon_7/1f.lua
@@ -8,7 +8,7 @@ separator_manager:manage_map(map)
 
 function map:on_started(destination)
 
-  if destination ~= from_2f_se then
+  if destination == from_2f_se then
     map:set_doors_open("auto_door_b", true)
   end
 end
