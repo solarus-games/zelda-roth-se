b6825993b76560fa75173f09e500f75cb7d501cf
Christopho <christopho@solarus-games.org>
2015-08-13 22:45:35 +0200

    Fix harmless error message in Mudora cave

--- data/maps/caves/mudora_cave/1f.lua
+++ data/maps/caves/mudora_cave/1f.lua
@@ -3,10 +3,3 @@ local game = map:get_game()
 
 local separator_manager = require("maps/lib/separator_manager")
 separator_manager:manage_map(map)
-
-function close_door_a_sensor:on_activated()
-
-  if door_a:is_open() then
-    map:close_doors("door_a")
-  end
-end
