815a029cf9f4725278960f49083c8035e591a871
Christopho <christopho@solarus-games.org>
2015-08-13 21:58:49 +0200

    Make bad skulls step back also when unsuccessfully attacked
    Closes #53

--- data/enemies/stal.lua
+++ data/enemies/stal.lua
@@ -1,6 +1,6 @@
 local enemy = ...
 
--- A vulture that sleeps until the hero gets close.
+-- A skull that wakes when the hero gets close.
 
 local behavior = require("enemies/lib/waiting_for_hero")
 
@@ -17,10 +17,25 @@ behavior:create(enemy, properties)
 
 -- Only the hammer can hurt this enemy.
 enemy:set_invincible()
-enemy:set_attack_consequence("sword", "protected")
-enemy:set_attack_consequence("thrown_item", "protected")
-enemy:set_attack_consequence("boomerang", "protected")
-enemy:set_arrow_reaction("protected")
-enemy:set_hammer_reaction("protected")
-enemy:set_hookshot_reaction("protected")
+enemy:set_attack_consequence("sword", "custom")
+enemy:set_attack_consequence("thrown_item", "custom")
+enemy:set_attack_consequence("boomerang", "custom")
+enemy:set_arrow_reaction("custom")
+enemy:set_hookshot_reaction("custom")
 enemy:set_hammer_reaction(1)
+
+function enemy:on_custom_attack_received(attack)
+
+  -- Custom reaction: don't get hurt but step back.
+  sol.timer.stop_all(enemy)  -- Stop the towards_hero behavior.
+  local hero = enemy:get_map():get_hero()
+  local angle = hero:get_angle(enemy)
+  local movement = sol.movement.create("straight")
+  movement:set_speed(128)
+  movement:set_ignore_obstacles(properties.ignore_obstacles)
+  movement:set_angle(angle)
+  movement:start(enemy)
+  sol.timer.start(enemy, 150, function()
+    enemy:restart()
+  end)
+end
